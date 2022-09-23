725c70ffb016fc4d212ee2b9cd9269f201b7f181
YonatanCherrez <112219904+YonatanCherrez@users.noreply.github.com>
2022-09-23 10:42:28 -0500

    Ajuste del código

--- main.ts
+++ main.ts
@@ -3,13 +3,12 @@ basic.forever(function () {
     while (input.temperature() < 26) {
         basic.showString("T. Baja")
     }
-    while (input.temperature() >= 26) {
+    basic.showNumber(input.temperature())
+    while (input.temperature() <= 26) {
         basic.showString("T. Normal")
     }
-    while (input.temperature() == 34) {
-        basic.showString("T. Normal")
-    }
-    while (input.temperature() > 34) {
+    basic.showNumber(input.temperature())
+    while (input.temperature() >= 34) {
         basic.showString("T. Alta")
     }
 })
